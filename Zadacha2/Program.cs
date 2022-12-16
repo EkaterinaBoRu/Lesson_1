d90c6c38e6c54f216bb98023cc7142cef427cac7
ekaterina <rudkate21@mail.ru>
2022-12-16 22:51:41 +0300

    create

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число 1: ");
+int number_1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число 2: ");
+int number_2 = int.Parse(Console.ReadLine()!);
+
+if (number_1 > number_2)
+{
+    Console.WriteLine($"Большее число: {number_1}, меньшее число: {number_2}");
+}
+else if (number_2 > number_1)
+{
+    Console.WriteLine($"Большее число: {number_2}, меньшее число: {number_1}");
+}
+else Console.WriteLine($"Числа равны");
---
+++ Zadacha4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число 1: ");
+int number_1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число 2: ");
+int number_2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число 3: ");
+int number_3 = int.Parse(Console.ReadLine()!);
+
+if (number_1 >= number_2)
+{
+    if (number_1 >= number_3)
+    {
+        Console.WriteLine($"Наибольшее число: {number_1}");
+    }
+    else Console.WriteLine($"Наибольшее число: {number_3}");
+}
+else if (number_2 >= number_3)
+    {
+        Console.WriteLine($"Наибольшее число: {number_2}");
+    }   
+else Console.WriteLine($"Наибольшее число: {number_3}");
+
